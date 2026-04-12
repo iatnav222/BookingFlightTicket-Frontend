c90ea31d261ee39820cdc777357466f3b019d779
Nguyễn Hoàng Gia Bảo <nhgbao12345@gmail.com>
2026-04-12 20:27:55 +0700

    Update: Order và tài khoản

--- src/services/GiaveApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const API_URL = 'https://bookingflightticket-backend-new.onrender.com/api/admin';
-
-export const priceApi = {
-    getDanhSach: (params) => axios.get(`${API_URL}/gia-ve`, { params }),
-    getChiTiet: (id) => axios.get(`${API_URL}/gia-ve/${id}`),
-    capNhatGia: (id, data) => axios.put(`${API_URL}/gia-ve/${id}`, data),
-    getChuyenBay: () => axios.get(`${API_URL}/chuyen-bay`), 
-};
